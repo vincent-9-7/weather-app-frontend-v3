d13b759dc1689855d6dba70d97d41d7ece7f1acf
ww <415578597@qq.com>
2021-02-24 19:23:46 +1000

    空格

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const mapboxKey = process.env.REACT_APP_MAPBOX_KEY;
-const weatherKey = process.env.REACT_APP_WEATHER_KEY;
-const googleKey = process.env.REACT_APP_GOOGLE_KEY;
+const mapboxKey = process.env.REACT_APP_MAPBOX_KEY
+const weatherKey = process.env.REACT_APP_WEATHER_KEY
+const googleKey = process.env.REACT_APP_GOOGLE_KEY
 
 
 export const getLocationApi = (currentCity) =>
     `https://api.mapbox.com/geocoding/v5/mapbox.places`
     +
-    `/${currentCity}.json?access_token=${mapboxKey}`;
+    `/${currentCity}.json?access_token=${mapboxKey}`
 
 
 export const getWeatherApi = (lat,lng) =>
@@ -16,26 +16,26 @@ export const getWeatherApi = (lat,lng) =>
     +
     `onecall?lat=${lat}&lon=${lng}&exclude=minutely,hourly,alerts`
         +
-    `&appid=${weatherKey}&units=metric`;
+    `&appid=${weatherKey}&units=metric`
 
 
 export const getGMTApi = (lat,lng) =>
     `https://api.openweathermap.org/data/2.5/weather?`
     +
-    `lat=${lat}&lon=${lng}&appid=${weatherKey}`;
+    `lat=${lat}&lon=${lng}&appid=${weatherKey}`
 
 
 export const getTimeApi = (GMT) =>
-    `https://worldtimeapi.org/api/timezone/Etc/GMT${GMT}`;
+    `https://worldtimeapi.org/api/timezone/Etc/GMT${GMT}`
 
 export const getTimeApiGMT0 = () =>
-    `https://worldtimeapi.org/api/timezone/Etc/GMT`;
+    `https://worldtimeapi.org/api/timezone/Etc/GMT`
 
 
 export const getLocationGoogleApi = (currentCity) =>
     `https://maps.googleapis.com/maps/api/geocode/`
     +
-    `json?address=${currentCity}&key=${googleKey}`;
+    `json?address=${currentCity}&key=${googleKey}`
 
 // export{
 //     getLocationApi,
